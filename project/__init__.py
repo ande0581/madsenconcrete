dc430c0b5a7981354c22cc56ec98fff2e4c921ab
Jeff Anderson <jeffrey.d.anderson@gmail.com>
2016-01-23 13:42:04 -0600

    add calculator blueprint

--- project/__init__.py
+++ project/__init__.py
@@ -10,6 +10,7 @@ db = SQLAlchemy(app)
 
 from project.address.views import address_blueprint
 from project.bid.views import bid_blueprint
+from project.calculator.views import calculator_blueprint
 from project.customer.views import customer_blueprint
 from project.item.views import item_blueprint
 from project.journal.views import journal_blueprint
@@ -20,6 +21,7 @@ from project.user.views import user_blueprint
 # register our blueprints
 app.register_blueprint(address_blueprint)
 app.register_blueprint(bid_blueprint)
+app.register_blueprint(calculator_blueprint)
 app.register_blueprint(customer_blueprint)
 app.register_blueprint(item_blueprint)
 app.register_blueprint(journal_blueprint)
