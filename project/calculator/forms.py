21df886e5ddada5930ffb75f75d1fe8d22a9fe64
Jeff Anderson <jeffrey.d.anderson@gmail.com>
2016-01-23 16:51:15 -0600

    add new fields to calculator form to get the area of a cylinder, also added quantity to the slab/square footings form

--- project/calculator/forms.py
+++ project/calculator/forms.py
@@ -6,8 +6,12 @@ from wtforms import StringField, FloatField
 from wtforms.validators import DataRequired, Length, Email, Optional
 
 
-class CalculateSlab(Form):
+class CalculatorForm(Form):
     slab_thickness = FloatField('slab_thickness', validators=[Optional()])
     slab_width = FloatField('slab_width', validators=[Optional()])
     slab_length = FloatField('slab_length', validators=[Optional()])
+    slab_quantity = FloatField('slab_quantity', validators=[Optional()])
+    round_diameter = FloatField('round_diameter', validators=[Optional()])
+    round_depth = FloatField('round_depth', validators=[Optional()])
+    round_quantity = FloatField('round_quantity', validators=[Optional()])
 
