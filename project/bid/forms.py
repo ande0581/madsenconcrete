f678dde04a1b3a626b22f20d20f06979d43fb606
Jeff Anderson <jeffrey.d.anderson@gmail.com>
2016-01-30 06:41:28 -0600

    add notes, down payment date/amount, paid in full date/amount to forms

--- project/bid/forms.py
+++ project/bid/forms.py
@@ -2,7 +2,7 @@
 
 
 from flask_wtf import Form
-from wtforms import DateField, DateTimeField, SelectField, TextAreaField
+from wtforms import DateField, DateTimeField, SelectField, TextAreaField, DecimalField
 from wtforms.validators import DataRequired, Optional
 
 
@@ -13,6 +13,7 @@ class AddBidForm(Form):
 
 class EditBidForm(Form):
     description = TextAreaField('description', validators=[DataRequired()])
+    notes = TextAreaField('description', validators=[Optional()])
     status = SelectField(
         'Status',
         validators=[DataRequired()],
@@ -24,3 +25,7 @@ class EditBidForm(Form):
     tentative_start = DateField('Tentative Start Date (yyyy-mm-dd)', validators=[Optional()])
     actual_start = DateField('Actual Start Date (yyyy-mm-dd)', validators=[Optional()])
     completion_date = DateField('Completion Date (yyyy-mm-dd)', validators=[Optional()])
+    down_payment_amount = DecimalField('Down Payment Amount', validators=[Optional()])
+    down_payment_date = DateField('Tentative Start Date (yyyy-mm-dd)', validators=[Optional()])
+    paid_in_full_amount = DecimalField('Down Payment Amount', validators=[Optional()])
+    paid_in_full_date = DateField('Actual Start Date (yyyy-mm-dd)', validators=[Optional()])
