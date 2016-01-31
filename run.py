75e1beb2e6de2a11df3e2fb32e54676823230a8d
Jeff Anderson <jeffrey.d.anderson@gmail.com>
2016-01-31 13:20:19 -0600

    removed heroku to different repository. Deleting specific heroku config

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
 # project/run.py
-import os
 from project import app
 
-""" when running locally
 app.run(debug=True)
-"""
-
-# when running on Heroku
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
