cb9d5147d41f4d6cf5984cb28a6868d629a2ebd8
Jeff Anderson <jeffrey.d.anderson@gmail.com>
2016-03-02 20:01:39 -0600

    make zip code optional

--- project/address/forms.py
+++ project/address/forms.py
@@ -3,11 +3,11 @@
 
 from flask_wtf import Form
 from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
 
 
 class AddAddressForm(Form):
     street = StringField('Street', validators=[DataRequired()])
     city = StringField('City', validators=[DataRequired()])
     state = StringField('State', validators=[DataRequired(), Length(min=2, max=2)])
-    zip = StringField('Zip', validators=[DataRequired(), Length(min=5, max=5)])
+    zip = StringField('Zip', validators=[Optional(), Length(min=5, max=5)])
