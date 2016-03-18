96104c9bf6369685757959c69e0f4c6c2158fbb9
Jeff Anderson <jeffrey.d.anderson@gmail.com>
2016-03-17 20:41:08 -0500

    refactor overview page to do almost all logic in python before rendering html to improve page performance. Not done but the basics are working.

--- project/overview/views.py
+++ project/overview/views.py
@@ -7,6 +7,9 @@ from flask import flash, redirect, render_template, session, url_for, Blueprint
 from project import db
 from project.models import Customer, Address, Bid
 
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
 
 ##########
 # config #
@@ -44,22 +47,6 @@ def query_all_addresses():
     return db.session.query(Address)
 
 
-def query_jobs_need_bid():
-    return db.session.query(Bid).filter_by(status='Needs Bid').order_by(Bid.scheduled_bid_date.asc())
-
-
-def query_jobs_started():
-    return db.session.query(Bid).filter_by(status='Job Started').order_by(Bid.actual_start.asc())
-
-
-def query_jobs_accepted():
-    return db.session.query(Bid).filter_by(status='Job Accepted').order_by(Bid.tentative_start.asc())
-
-
-def query_jobs_awaiting_acceptance():
-    return db.session.query(Bid).filter_by(status='Awaiting Customer Acceptance').order_by(Bid.scheduled_bid_date.desc())
-
-
 ##########
 # Routes #
 ##########
@@ -69,12 +56,54 @@ def query_jobs_awaiting_acceptance():
 @overview_blueprint.route('/overview/')
 @login_required
 def overview():
-    error = None
-    return render_template('overview.html',
-                           jobs_need_bid=query_jobs_need_bid(),
-                           jobs_started=query_jobs_started(),
-                           jobs_accepted=query_jobs_accepted(),
-                           jobs_awaiting_acceptance=query_jobs_awaiting_acceptance(),
-                           customers=query_all_customers(),
-                           addresses=query_all_addresses(),
-                           error=error)
+
+    needs_bid = db.session.query(Bid).filter_by(status='Needs Bid').order_by(Bid.scheduled_bid_date.asc())
+    job_started = db.session.query(Bid).filter_by(status='Job Started').order_by(Bid.actual_start.asc())
+    job_accepted = db.session.query(Bid).filter_by(status='Job Accepted').order_by(Bid.tentative_start.asc())
+    awaiting_acceptance = db.session.query(Bid).filter_by(status='Awaiting Customer Acceptance').order_by(Bid.scheduled_bid_date.desc())
+
+    all_databases = [needs_bid, job_started, job_accepted, awaiting_acceptance]
+
+    # combine customer and address and bid together
+    database_list = []
+    for one_database in all_databases:
+        for bid in one_database:
+            bid_info = dict()
+            bid_info['customer_name'] = db.session.query(Customer.name).filter_by(id=bid.customer_id).first()
+            bid_info['customer_name'] = bid_info['customer_name'][0]
+            bid_info['customer_id'] = bid.customer_id
+            bid_info['bid_description'] = bid.description
+            bid_info['bid_status'] = bid.status
+            bid_info['bid_id'] = bid.id
+
+            if bid_info['bid_status'] == 'Needs Bid':
+                bid_info['bid_date'] = bid.scheduled_bid_date.strftime('%x')
+            elif bid_info['bid_status'] == 'Job Started':
+                bid_info['bid_date'] = bid.actual_start.strftime('%x')
+            elif bid_info['bid_status'] == 'Job Accepted':
+                bid_info['bid_date'] = bid.tentative_start.strftime('%x')
+            elif bid_info['bid_status'] == 'Awaiting Customer Acceptance':
+                bid_info['bid_date'] = bid.scheduled_bid_date.strftime('%x')
+            else:
+                bid_info['bid_date'] = "Something didn't work right!!"
+
+            bid_info['address_street'] = db.session.query(Address.street).filter_by(customer_id=bid.customer_id).first()
+            bid_info['address_street'] = bid_info['address_street'][0]
+            bid_info['address_city'] = db.session.query(Address.city).filter_by(customer_id=bid.customer_id).first()
+            bid_info['address_city'] = bid_info['address_city'][0]
+            bid_info['address_state'] = db.session.query(Address.state).filter_by(customer_id=bid.customer_id).first()
+            bid_info['address_state'] = bid_info['address_state'][0]
+            bid_info['address_zip'] = db.session.query(Address.zip).filter_by(customer_id=bid.customer_id).first()
+            bid_info['address_zip'] = bid_info['address_zip'][0]
+
+            telephone = db.session.query(Customer.telephone).filter_by(id=bid.customer_id).first()
+            if telephone[0]:
+                bid_info['customer_telephone'] = "({}) {}-{}".format(telephone[0][:3], telephone[0][3:6], telephone[0][6:])
+            else:
+                bid_info['customer_telephone'] = ""
+
+            database_list.append(bid_info)
+
+    pp.pprint(database_list)
+
+    return render_template('overview_test.html', bids=database_list)
